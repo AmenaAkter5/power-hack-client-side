3a380506638d89441730712db1975e215e63dea5
unknown <latapata1952@gmail.com>
2022-07-01 16:09:42 +0600

    get billing data api fetch successfully

--- src/components/Home/Table.js
+++ src/components/Home/Table.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const Table = () => {
+
+    const [bills, setBills] = useState([]);
+
+    useEffect(() => {
+
+        fetch(`http://localhost:5000/api/billing-list`)
+
+            .then(res => {
+
+                if (res.status === 401 || res.status === 403) {
+
+                    /* signOut(auth);
+                    localStorage.removeItem('accessToken');
+                    navigate('/'); */
+                }
+
+                return res.json();
+            })
+
+            .then(data => {
+                setBills(data)
+            })
+
+    }, [bills]);
+
     return (
         <div class="overflow-x-auto w-11/12 mx-auto">
             <table class="table w-full">
-                {/* <!-- head --> */}
                 <thead>
                     <tr>
                         <th>Billing Id</th>
@@ -16,27 +41,34 @@ const Table = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* <!-- row 1 --> */}
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    {/* <!-- row 2 --> */}
-                    <tr>
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
-                    {/* <!-- row 3 --> */}
-                    <tr>
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                    </tr>
+                    {
+                        bills.map((bill, index) => <tr key={index}>
+                            <th>{bill._id}</th>
+                            <td>{bill.name}</td>
+                            <td>{bill.email}</td>
+                            <td>{bill.phone}</td>
+                            <td>{bill.amount}</td>
+                            <td>
+                                {/* {(bill.price && !bill.paid) && <p className=' text-red-600 font-bold'>Unpaid</p>}
+                                {(bill.price && bill.paid && !bill.shipment) && <div>
+                                    <span className='text-green-600 font-bold'>Pending</span>
+                                    <p>Transaction Id: <span className='text-orange-600'>{bill.transactionId}</span></p>
+                                </div>}
+                                {(bill.price && bill.paid && bill.shipment) && <div>
+                                    <p className='text-primary font-bold'>Shipped</p>
+                                </div>} */}
+                            </td>
+                            <td>
+                                {/* {(bill.price && bill.paid && !bill.shipment) && <button
+                                    onClick={() => handleShipment(bill._id)}
+                                    className='btn btn-xs btn-primary text-white font-bold'>Shipment</button>}
+                                {
+                                    (bill.price && !bill.paid && !bill.shipment) &&
+                                    <label htmlFor="delete-modal" onClick={() => setOrder(bill)} className='btn btn-xs btn-error text-white font-bold'>Cancel</label>
+                                } */}
+                            </td>
+                        </tr>)
+                    }
                 </tbody>
             </table>
         </div>
