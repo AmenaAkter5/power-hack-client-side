07dc2cfa0350584082b47fdf9c661d7d356eb361
unknown <latapata1952@gmail.com>
2022-07-01 12:59:52 +0600

    home added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
       </Routes>
