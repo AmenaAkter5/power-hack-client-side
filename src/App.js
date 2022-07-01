6756d325bfb19ba3e150d0085043c76c2fdf7f72
unknown <latapata1952@gmail.com>
2022-07-01 12:08:00 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import SignUp from './components/Login/SignUp';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
-      <SignUp></SignUp>
-      <Login></Login>
+      <Routes>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        powertheme: {
+          primary: "#4907b3",
+          secondary: "#fb17d0",
+          accent: "#f4f4f6",
+          neutral: "#154E64",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
